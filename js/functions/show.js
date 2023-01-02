5c8b234bbee663eaa5a1179769c47972c54b8acb
tdmdgh <tdmdgh@gmail.com>
2023-01-03 02:07:59 +0900

    feat: function show$all 생성

---
+++ js/functions/show.js
@@ -0,0 +1,6 @@
+function all(todos) {
+    for(i in todos){
+        console.log(todos[i])
+    }
+}
+module.exports.all = all;
--- js/todos.js
+++ js/todos.js
@@ -1,1 +1,19 @@
+var show = require("./functions/show");
+const todos =  [ 
+    {
+        'name' : '자바스크립트 공부하기', 
+        'tags' : ['programming', 'javascript'],
+        'status' : 'todo',
+        'id' : 12123123
+    },
+                    {
+        'name' : ' 그림 그리기', 
+        'tags' : ['picture', 'favorite'],
+        'status' : 'doing',
+        'id' : 312323
+    }
+]
+
+
+show.all(todos)
 console.log("hello world")

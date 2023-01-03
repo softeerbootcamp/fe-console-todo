f3b4d290025509b29eb9ed831ce78605242d064b
miinjoo <minjoo9903@ewhain.net>
2023-01-03 12:51:52 +0900

    fix:id없는경우 수정

--- delete.js
+++ delete.js
@@ -2,14 +2,19 @@ const todos = require('./todos.js');
 const show = require('./show.js');
 
 function remove(arr,id){
+    var check=0;
     arr.map(item => {
         if (item['id'] == id) {
             console.log(`${item['name']} ${item['status']}가 목록에서 삭제됐습니다.`);
             var index=todos.findIndex(i=>item['id']==id);
             todos.splice(index, 1);
             show('all');
+            check++;
             }
       });
+    if (check==0){
+        console.log('일치하는 id가 없습니다.')
+    }
 }
 
 module.exports=remove;
--- update.js
+++ update.js
@@ -1,14 +1,15 @@
-const arrCount=require('./arrCount.js');
 const show = require('./show.js');
-const todos=require('./todos.js')
 
 function update(arr,id, status){
+    var check=0;
     arr.map(item => {
         if (item['id'] == id) {
             item['status']=status;
             console.log(`${item['name']}가 ${status}으로 상태가 변경됐습니다. `);
             show('all');
+            check++;
             }
       });
+    if (check==0){console.log('일치하는 id가 없습니다.');}
 }
 module.exports= update;

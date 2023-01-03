4f5a79fa556146bb96d77572a9d0f15f02b46351
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-01-03 12:35:15 +0900

    Refactor : private property 생성

--- todo.js
+++ todo.js
@@ -1,64 +1,82 @@
 class Todo {
+    #todos;
+    #todo;
+    #doing;
+    #done;
+
     constructor(todos) {
-      this.todos = todos;
-      this.todo = [];
-      this.doing = [];
-      this.done = [];
+
+      this.#todos = todos;
+      this.#todo = [];
+      this.#doing = [];
+      this.#done = [];
       this._divideStatus();
+
     }
-  
+
     _divideStatus() {
-      this.todo = this.todos.filter((res) => res.status === 'todo');
-      this.doing = this.todos.filter((res) => res.status === 'doing');
-      this.done = this.todos.filter((res) => res.status === 'done');
+
+      this.#todo = this.#todos.filter((res) => res.status === 'todo');
+      this.#doing = this.#todos.filter((res) => res.status === 'doing');
+      this.#done = this.#todos.filter((res) => res.status === 'done');
+
     }
-  
+
     _validate(id) {
-      this.todos.forEach((todo) => {
+
+      this.#todos.forEach((todo) => {
+
         if (todo.id === id) return false;
       });
-  
+
       return true;
     }
-  
+
     printAll() {
-      return console.log(`현재상태 :  todo: ${this.todo.length}개, doing: ${this.doing.length}, done: ${this.done.length}`);
+
+      return console.log(`현재상태 :  todo: ${this.#todo.length}개, doing: ${this.#doing.length}, done: ${this.#done.length}`);
+    
     }
-  
+
     printType(type) {
+
       let tmp = [];
-  
-      type === 'todo' && (tmp = this.todo);
-      type === 'doing' && (tmp = this.doing);
-      type === 'done' && (tmp = this.done);
-  
+
+      type === 'todo' && (tmp = this.#todo);
+      type === 'doing' && (tmp = this.#doing);
+      type === 'done' && (tmp = this.#done);
+
       return console.log(`${type}리스트 :  총${tmp.length}건 :` + tmp.map((info) => ` '${info.name} , ${info.id}' `));
     }
-  
+
     add(name, tags) {
+
       const rand = Math.floor(Math.random() * 100000) + 1;
-  
+
       if (!this._validate(rand)) this.add(text, arr);
-  
+
       const newTodo = {
         name: name,
         tags: tags,
         status: 'todo',
         id: rand,
       };
-      this.todo.push(newTodo);
-      this.todos.push(newTodo);
-  
+
+      this.#todo.push(newTodo);
+      this.#todos.push(newTodo);
+
       console.log(`${name} 1개가 추가됐습니다.(id : ${rand})`);
-  
+
       return this.printAll();
     }
-  
+
     remove(id) {
+
       let name = '',
         type = '';
-  
-      this.todos = this.todos.filter((todo) => {
+
+      this.#todos = this.#todos.filter((todo) => {
+
         if (parseInt(id) === todo.id) {
           name = todo.name;
           type = todo.status;
@@ -66,31 +84,32 @@ class Todo {
         }
         return true;
       });
-  
+
       this._divideStatus();
       console.log(`${name} ${type}가 목록에서 삭제됐습니다`);
-  
+
       return this.printAll();
     }
-  
+
     update(id, type) {
+
       let name = '';
-  
-      this.todos.forEach((todo) => {
+
+      this.#todos.forEach((todo) => {
+
         if (todo.id === parseInt(id)) {
           todo.status = type;
           name = todo.name;
-  
           return;
         }
       });
-  
+
       this._divideStatus();
+
       console.log(`${name}가 ${type}으로 상태가 변경됐습니다`);
-  
+
       return this.printAll();
     }
   }
   
   module.exports = Todo;
-  

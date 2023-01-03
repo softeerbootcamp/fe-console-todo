51f041b95b3d10f60eccaab12fe31e81c242283c
RentalHub <rentalhub@RentalHubui-noteubug.local>
2023-01-03 11:30:38 +0900

    style: 오타 수정

--- constants.js
+++ constants.js
@@ -2,8 +2,8 @@ const MESSAGE = {
   ENTER_COMMAND: "명령하세요 :",
   CURRENT_STATUS: "현재상태 :",
   TODO_LIST: "todo리스트 :",
-  ADD_MESSEAGE: (name, id) => `${name} 1개가 추가됐습니다. (id : ${id})`,
-  DELETE_MESSEAGE: (name, status) =>
+  ADD_MESSAGE: (name, id) => `${name} 1개가 추가됐습니다. (id : ${id})`,
+  DELETE_MESSAGE: (name, status) =>
     `${name} ${status}가 목록에서 삭제됐습니다.`,
 };
 const COMMAND = {

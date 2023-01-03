a22b9284775ed6e2caf26874eb24779d62611db8
RentalHub <rentalhub@RentalHubui-noteubug.local>
2023-01-03 11:31:30 +0900

    feat: delete 기능 구현

--- util/deleteUtil.js
+++ util/deleteUtil.js
@@ -1,3 +1,18 @@
+const { findIdx } = require("./util");
+const { MESSAGE } = require("../constants");
+const { printCurrentStatus } = require("./util");
+
 function handleDelete(todos, middle) {
   const id = parseInt(middle);
+  const idx = findIdx(todos, id);
+  const name = todos[idx].name;
+  const status = todos[idx].status;
+
+  if (idx !== -1) {
+    todos.splice(idx, 1);
+    console.log(MESSAGE.DELETE_MESSAGE(name, status));
+  }
+  printCurrentStatus(todos);
 }
+
+module.exports = { handleDelete };
--- util/util.js
+++ util/util.js
@@ -1,6 +1,6 @@
-const { STATUS, STATUS_IDX, MESSAGE } = require('../constants');
+const { STATUS, STATUS_IDX, MESSAGE } = require("../constants");
 function parseCommand(cmd) {
-  return cmd.split('$');
+  return cmd.split("$");
 }
 
 function printCurrentStatus(todos) {
@@ -15,10 +15,14 @@ function printCurrentStatus(todos) {
 }
 
 function numOfStatus(todos) {
-  const numOfTodo = todos.filter(ele => ele.status === STATUS.TODO).length;
-  const numOfDoing = todos.filter(ele => ele.status === STATUS.DOING).length;
-  const numOfDone = todos.filter(ele => ele.status === STATUS.DONE).length;
+  const numOfTodo = todos.filter((ele) => ele.status === STATUS.TODO).length;
+  const numOfDoing = todos.filter((ele) => ele.status === STATUS.DOING).length;
+  const numOfDone = todos.filter((ele) => ele.status === STATUS.DONE).length;
   return [numOfTodo, numOfDoing, numOfDone];
 }
 
-module.exports = { printCurrentStatus, parseCommand };
+function findIdx(todos, id) {
+  return todos.findIndex((ele) => ele.id === id);
+}
+
+module.exports = { printCurrentStatus, parseCommand, findIdx };

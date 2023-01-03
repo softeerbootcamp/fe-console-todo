8de87bfab4691d08832316a591187af95c3c820e
RentalHub <rentalhub@RentalHubui-noteubug.local>
2023-01-03 14:32:35 +0900

    feat: addUtil getRandom 함수 구현

--- util/addUtil.js
+++ util/addUtil.js
@@ -1,9 +1,9 @@
-const { STATUS, MESSAGE } = require('../constants');
+const { STATUS, MESSAGE, MAX_TODOS } = require('../constants');
 const { printCurrentStatus } = require('../View/OutputView');
 const { printAddMessage } = require('../View/OutputView');
 
 function handleAdd(todos, name, tags) {
-  const id = getRandom();
+  const id = getRandom(todos);
   todos.push({
     name,
     tags: parseTags(tags),
@@ -21,7 +21,14 @@ function parseTags(tags) {
   return tags.split(',');
 }
 
-function getRandom() {
-  return Math.random();
+function getRandom(todos) {
+  const arr = [];
+  todos.forEach((ele) => {
+    arr[ele.id] = 1;
+  });
+  for (let i = 0; i < arr.length; i += 1) {
+    if (!arr[i]) return i;
+  }
+  return arr.length;
 }
 module.exports = { handleAdd };

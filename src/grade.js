818602188b333e7b1a1f8c4bf77df98336d69bc5
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-01-03 17:50:44 +0900

    Style : 출력문자 개행 추가

--- src/grade.js
+++ src/grade.js
@@ -3,20 +3,20 @@ const input = require('./input');
 const Todo = require('./Todo');
 
 const playTodo = async () => {
-  console.log('종료를 원하시면 0을 입력하세요!');
+  console.log('종료를 원하시면 0을 입력하세요!\n');
 
   while (true) {
     const query = await input();
-    const [command, typeOrID, tagOrStatus] = [...query];
+    const [command, typeOrId, tagOrStatus] = [...query];
 
     if (command === 'show') {
-      typeOrID === 'all' ? TodoList.printAll() : TodoList.printType(typeOrID);
+      typeOrId === 'all' ? TodoList.printAll() : TodoList.printType(typeOrId);
     } else if (command === 'add') {
-      TodoList.add(typeOrID, tagOrStatus);
+      TodoList.add(typeOrId, tagOrStatus);
     } else if (command === 'delete') {
-      TodoList.remove(typeOrID);
+      TodoList.remove(typeOrId);
     } else if (command === 'update') {
-      TodoList.update(typeOrID, tagOrStatus);
+      TodoList.update(typeOrId, tagOrStatus);
     }
   }
 };

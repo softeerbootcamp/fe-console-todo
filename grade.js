0bd882ab5c3de02b5f7e646984ea36845efcabac
RentalHub-MAC23 <king4mun@gmail.com>
2023-01-04 12:14:37 +0900

    add: regular expression

--- grade.js
+++ grade.js
@@ -2,7 +2,7 @@ const { show } = require('./show');
 const { update } = require("./update");
 const { idelete } = require('./delete');
 const { addList } = require("./add");
-
+const {showReg, addReg, deleteReg, updateReg} = require("./regExp");
 const readline = require("readline");
 
 const reader = readline.createInterface({
@@ -10,24 +10,7 @@ const reader = readline.createInterface({
     output: process.stdout,
 });
 
-function selectJob(job, firstParam, secondParam) {
-    // delete
-    if(job === 'delete') {
-        idelete(firstParam);
-    }
-    // update
-    else if(job === 'update') {
-        update(firstParam, secondParam)
-    }
-    // add
-    else if(job === 'add') {
-        addList(firstParam, secondParam);
-    }
-
-    show("all");
-}
-
-reader.setPrompt('명령하세요: ("exit" to fisnish) ');
+reader.setPrompt('명령하세요 ("exit" to fisnish) : ');
 
 reader.prompt(); 
 
@@ -39,55 +22,32 @@ reader.on("line", (line) => {
     // 사용자 input 파싱하여 배열 저장
     let inputWords = line.split('$');
 
-    if(inputWords.length === 1) {
-        console.log("잘못된 입력입니다. 올바르게 입력해주세요. ");
-    }
     // show
-    else if(inputWords[0]=== 'show' && inputWords.length === 2){
+    if(showReg.test(line)) {
         show(inputWords[1]);
     }
-    // delete & update & add
-    else if(inputWords[0] === 'delete' || inputWords[0] === 'update' || inputWords[0] === 'add') {
-        selectJob(inputWords[0], inputWords[1], inputWords[2]);
+    // delete
+    else if(deleteReg.test(line)) {
+        idelete(inputWords[1]);
+        show("all");
     }
-    // except case
+    // add
+    else if(addReg.test(line)) {
+        addList(inputWords[1], inputWords[2]);
+        show("all");
+    }
+    // update
+    else if(updateReg.test(line)) {
+        update(inputWords[1], inputWords[2]);
+        show("all");
+    }
     else {
-        console.log("잘못된 입력입니다. 올바르게 입력해주세요. ");
+        console.log("잘못된 입력입니다. 올바르게 입력해주세요.")
     }
 
     reader.prompt();
 });
 
-reader.on("close",function(){
+reader.on("close", () => {
     process.exit();
 })
-
-// 참고 부분
-// readline의 question 함수를 통해 무한 반복하는 방법
-/*
-onst readline = require("readline");
-
-const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-function getInput() {
-    reader.question("명령어를 입력하세요 : ", (line) => {
-    
-        if(line == 'exit') {
-            reader.close();
-    
-            reader.on("close",function(){
-                process.exit();
-            })
-        }
-        else {
-            console.log("출력 부분 : ", line)
-            getInput();
-        }
-    })   
-}
-
-getInput();
-*/
